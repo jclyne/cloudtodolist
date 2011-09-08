3084a1cd4ea2c68a13ef1415f4e4cfb726f23f0f
Jeff Clyne <jeffclyne@mindless.com>
2011-09-07 21:37:18 -0500

    Added ListDataProvider and removed seperate client class

--- todolist_client/src/com/oci/example/todolist/client/TodoList.java
+++ todolist_client/src/com/oci/example/todolist/client/TodoList.java
@@ -1,18 +1,30 @@
 package com.oci.example.todolist.client;
 
-import com.google.gwt.cell.client.*;
+import com.google.gwt.cell.client.CheckboxCell;
+import com.google.gwt.cell.client.FieldUpdater;
+import com.google.gwt.cell.client.TextInputCell;
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.core.client.JsArray;
+import com.google.gwt.core.client.JsArrayNumber;
+import com.google.gwt.dom.client.Style;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.cellview.client.*;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.http.client.*;
+import com.google.gwt.user.cellview.client.CellTable;
+import com.google.gwt.user.cellview.client.Column;
+import com.google.gwt.user.cellview.client.HasKeyboardSelectionPolicy;
 import com.google.gwt.user.client.Timer;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.*;
+import com.google.gwt.view.client.ListDataProvider;
+import com.google.gwt.view.client.NoSelectionModel;
 import com.google.gwt.view.client.ProvidesKey;
-import com.google.gwt.view.client.SingleSelectionModel;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 
@@ -21,111 +33,242 @@ import java.util.List;
  */
 public class TodoList implements EntryPoint {
 
-    /**
-     * The key provider that allows us to identify Contacts even if a field
-     * changes. We identify contacts by their unique ID.
-     */
+    private static final String TODOLIST_BASE_URL = "http://" + Window.Location.getHost() + "/todolist/api/";
+    private static final String GET_ENTRY_URL = TODOLIST_BASE_URL + "getentry?";
+    private static final String SET_ENTRY_URL = TODOLIST_BASE_URL + "setentry?";
+    private static final String DEL_ENTRY_URL = TODOLIST_BASE_URL + "delentry?";
+
     private static final ProvidesKey<TodoListEntry> todoListEntryKeyProvider = new ProvidesKey<TodoListEntry>() {
         public Object getKey(TodoListEntry entry) {
             return entry.getId();
         }
     };
 
-    private final SingleSelectionModel<TodoListEntry> singleEntrySelectionModel = new SingleSelectionModel<TodoListEntry>();
+    private final NoSelectionModel<TodoListEntry> noEntrySelectionModel = new NoSelectionModel<TodoListEntry>();
+    private final ListDataProvider<TodoListEntry> todoListDataProvider = new ListDataProvider<TodoListEntry>();
 
     private final VerticalPanel mainPanel = new VerticalPanel();
     private final CellTable<TodoListEntry> todoList = new CellTable<TodoListEntry>(todoListEntryKeyProvider);
-    private final CellTable<TodoListEntry> newEntry = new CellTable<TodoListEntry>();
     private final HorizontalPanel toolPanel = new HorizontalPanel();
     private final Button refreshListButton = new Button("Refresh");
     private final Button clearCompletedButton = new Button("Clear Completed");
+    private final TextBox newEntry = new TextBox();
+    private final InlineLabel statusLabel = new InlineLabel("");
 
-    private TodoListDataClient client;
-    private static final int POLL_INTERVAL = 5000;
+
+    private static final int POLL_INTERVAL = 1000;
 
     public void onModuleLoad() {
-        client = new TodoListDataClient(new ClientRequestCallback() {
-
-            @Override
-            public void onSuccess() {
-                redrawTable();
-            }
-
-            @Override
-            public void onError(String errStr) {
-
-            }
-        });
 
         todoList.setKeyboardSelectionPolicy(HasKeyboardSelectionPolicy.KeyboardSelectionPolicy.ENABLED);
-        todoList.setSelectionModel(singleEntrySelectionModel);
-        todoList.addColumn(buildCheckCompleteColumn(),new TextHeader(""));
+        todoList.setSelectionModel(noEntrySelectionModel);
+
+        Column<TodoListEntry,Boolean> completeColumn = buildCheckCompleteColumn();
+        Column<TodoListEntry, String> titleColumn = buildEntryTitleColumn();
+
+        todoList.addColumn(completeColumn);
+        todoList.addColumn(titleColumn, "Todo List");
+
+        todoList.setWidth("75%", true);
+        todoList.setColumnWidth(completeColumn, 35.0, Style.Unit.PX);
+        todoList.setColumnWidth(titleColumn, 100.0, Style.Unit.PCT);
 
 
-        Header<String> newEntryFooter=
-                new Header<String>(new TextInputCell()) {
-                    @Override
-                    public String getValue() {
-                        return "";
-                    }
-                };
+        todoListDataProvider.addDataDisplay(todoList);
 
-        newEntryFooter.setUpdater(new ValueUpdater<String>() {
-            public void update(String value) {
-                client.createNewEntry(value);
+        newEntry.addValueChangeHandler(new ValueChangeHandler<String> (){
+            @Override
+            public void onValueChange(ValueChangeEvent<String> stringValueChangeEvent) {
+                String newTitle= stringValueChangeEvent.getValue();
+                newEntry.setText("");
+                String urlString = SET_ENTRY_URL + "title=" + newTitle;
+                RequestBuilder builder = new RequestBuilder(RequestBuilder.PUT, URL.encode(urlString));
+
+                try {
+                    builder.sendRequest(null, new RequestCallback() {
+                        public void onError(Request request, Throwable e) {
+                            statusLabel.setText("Server Error: " + e.toString());
+                        }
+
+                        private native JsArray<TodoListEntryResponse> parseGetEntryResponse(String json) /*-{
+                            return eval(json);
+                        }-*/;
+
+                        public void onResponseReceived(Request request, Response response) {
+                            if (response.getStatusCode() == 200) {
+                                List<TodoListEntry> todoListEntries = todoListDataProvider.getList();
+                                JsArray<TodoListEntryResponse> entries = parseGetEntryResponse(response.getText());
+                                for (int i = 0; i < entries.length(); i++) {
+                                    TodoListEntryResponse entry = entries.get(i);
+                                    todoListEntries.add(new TodoListEntry(entry.getId(),
+                                            entry.getTitle(),
+                                            entry.getNotes(),
+                                            entry.isComplete()));
+                                }
+                            } else {
+                                statusLabel.setText("Server Error: '"+response.getStatusCode() +"' "
+                                                    +response.getStatusText());
+                            }
+                        }
+                    });
+                } catch (RequestException e) {
+                    statusLabel.setText("Server Error: " + e.toString());
+                }
             }
         });
-
-        todoList.addColumn(buildEntryTitleColumn(),new TextHeader("Todo List"),newEntryFooter);
 
         // Listen for mouse events on the Add button.
         refreshListButton.addClickHandler(new ClickHandler() {
             public void onClick(ClickEvent event) {
-                client.refreshTodoListEntries();
+                refreshTodoListEntries();
             }
         });
 
         // Listen for mouse events on the clear completed
         clearCompletedButton.addClickHandler(new ClickHandler() {
             public void onClick(ClickEvent event) {
-                client.clearCompletedEntries();
+                String urlString = DEL_ENTRY_URL;
+                int count = 0;
+                for (TodoListEntry entry: todoListDataProvider.getList()) {
+                    if (entry.isComplete()) {
+                        if (count++ == 0) {
+                            urlString += "id=" + Integer.toString(entry.getId());
+                        } else {
+                            urlString += "+" + Integer.toString(entry.getId());
+                        }
+                    }
+                }
+
+                if (count > 0) {
+                    RequestBuilder builder = new RequestBuilder(RequestBuilder.PUT, URL.encode(urlString));
+                    try {
+                        builder.sendRequest(null, new RequestCallback(){
+                            public void onError(Request request, Throwable e) {
+                                statusLabel.setText("Server Error: " + e.toString());
+                            }
+
+                            private native JsArrayNumber parseDeleteEntryResponse(String json) /*-{
+                                return eval(json);
+                            }-*/;
+
+                            public void onResponseReceived(Request request, Response response) {
+                                if (response.getStatusCode() == 200) {
+                                    List<TodoListEntry> todoListEntries = todoListDataProvider.getList();
+                                    JsArrayNumber deletedEntries = parseDeleteEntryResponse(response.getText());
+                                    for (int i = 0; i < deletedEntries.length(); i++){
+                                        for (int j = 0; j < todoListEntries.size(); j++){
+                                            if (todoListEntries.get(j).getId() == (int)deletedEntries.get(i)) {
+                                                todoListEntries.remove(j);
+                                                break;
+                                            }
+                                        }
+                                    }
+                                    todoListDataProvider.refresh();
+                                } else {
+                                    statusLabel.setText("Server Error: '"+response.getStatusCode() +"' "
+                                                        +response.getStatusText());
+                                }
+                            }
+                        } );
+
+                    } catch (RequestException e) {
+                        statusLabel.setText("Server Error: " + e.toString());
+                    }
+                }
             }
         });
 
         toolPanel.add(refreshListButton);
         toolPanel.add(clearCompletedButton);
 
+        mainPanel.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_LEFT);
         mainPanel.add(todoList);
+        mainPanel.add(newEntry);
         mainPanel.add(toolPanel);
+        mainPanel.add(statusLabel);
 
         // Add it to the root panel.
         RootPanel.get().add(mainPanel);
 
-        client.refreshTodoListEntries();
-        //startUpdatePollingTimer();
+        refreshTodoListEntries();
+        startUpdatePollingTimer();
     }
 
-    private void redrawTable() {
-        List<TodoListEntry> entryData = client.getCurrentTodoListEntries();
-
-        todoList.setRowCount(entryData.size(), true);
-        todoList.setRowData(0, entryData);
-        todoList.redraw();
-    }
 
     private void startUpdatePollingTimer() {
         // Setup timer to refresh list automatically.
         Timer refreshTimer = new Timer() {
             @Override
             public void run() {
-                client.refreshTodoListEntries();
-                redrawTable();
+                refreshTodoListEntries();
             }
         };
         refreshTimer.scheduleRepeating(POLL_INTERVAL);
     }
 
-    private Column<TodoListEntry, ?> buildCheckCompleteColumn() {
+    final static class TodoListEntryResponse extends JavaScriptObject {
+        protected TodoListEntryResponse() {
+        }
+
+        public final native int getId() /*-{
+            return this.id;
+        }-*/;
+
+        public final native String getTitle() /*-{
+            return this.title;
+        }-*/;
+
+        public final native String getNotes() /*-{
+            return this.notes;
+        }-*/;
+
+        public final native boolean isComplete() /*-{
+            return this.complete == 1;
+        }-*/;
+    }
+
+
+    private void refreshTodoListEntries() {
+        String url = URL.encode(GET_ENTRY_URL);
+        RequestBuilder builder = new RequestBuilder(RequestBuilder.GET, url);
+
+        try {
+            builder.sendRequest(null, new RequestCallback() {
+                public void onError(Request request, Throwable e) {
+                    statusLabel.setText("Server Error: " + e.toString());
+                }
+
+
+                private native JsArray<TodoListEntryResponse> parseGetEntryResponse(String json) /*-{
+                    return eval(json);
+                }-*/;
+
+                public void onResponseReceived(Request request, Response response) {
+                    if (response.getStatusCode() == 200) {
+                        JsArray<TodoListEntryResponse> entries = parseGetEntryResponse(response.getText());
+                        List<TodoListEntry> newDisplayList = new ArrayList<TodoListEntry>();
+                        for (int i = 0; i < entries.length(); i++) {
+                            TodoListEntryResponse entry = entries.get(i);
+                            newDisplayList.add(new TodoListEntry(entry.getId(),
+                                    entry.getTitle(),
+                                    entry.getNotes(),
+                                    entry.isComplete()));
+                        }
+                        Collections.sort(newDisplayList, new TodoListEntry.CompareId());
+                        todoListDataProvider.setList(newDisplayList);
+                        todoListDataProvider.refresh();
+                    } else {
+                        statusLabel.setText("Server Error: '"+response.getStatusCode() +"' "
+                                                    +response.getStatusText());
+                    }
+                }
+            });
+        } catch (RequestException e) {
+            statusLabel.setText("Server Error: " + e.toString());
+        }
+    }
+
+    private Column<TodoListEntry, Boolean> buildCheckCompleteColumn() {
         // Create the column, mapping a TodoListEntry to the boolean
         Column<TodoListEntry, Boolean> column =
                 new Column<TodoListEntry, Boolean>(new CheckboxCell()) {
@@ -138,7 +281,27 @@ public class TodoList implements EntryPoint {
         // Add a field updater to be notified when the user clears an item
         column.setFieldUpdater(new FieldUpdater<TodoListEntry, Boolean>() {
             public void update(int index, TodoListEntry entry, Boolean value) {
-                client.setEntryComplete(entry.getId(), value);
+                entry.setComplete(value);
+                String urlString = SET_ENTRY_URL + "id=" + entry.getId() + ";complete=" + (value ? '1' : '0');
+                RequestBuilder builder = new RequestBuilder(RequestBuilder.PUT, URL.encode(urlString));
+
+                try {
+                    builder.sendRequest(null, new RequestCallback() {
+                        public void onError(Request request, Throwable e) {
+                            statusLabel.setText("Server Error: " + e.toString());
+                        }
+
+                        public void onResponseReceived(Request request, Response response) {
+                            if (response.getStatusCode() == 200) {
+                            } else {
+                                statusLabel.setText("Server Error: '"+response.getStatusCode() +"' "
+                                                    +response.getStatusText());
+                            }
+                        }
+                    });
+                } catch (RequestException e) {
+                    statusLabel.setText("Server Error: " + e.toString());
+                }
             }
         });
 
@@ -146,7 +309,7 @@ public class TodoList implements EntryPoint {
 
     }
 
-    private Column<TodoListEntry, ?> buildEntryTitleColumn() {
+    private Column<TodoListEntry, String> buildEntryTitleColumn() {
         // Create a modifiable title cell
         Column<TodoListEntry, String> column =
                 new Column<TodoListEntry, String>(new TextInputCell()) {
@@ -160,7 +323,27 @@ public class TodoList implements EntryPoint {
         // Add a field updater to be notified when the user enters a new name.
         column.setFieldUpdater(new FieldUpdater<TodoListEntry, String>() {
             public void update(int index, TodoListEntry entry, String value) {
-                client.setEntryTitle(entry.getId(), value);
+                entry.setTitle(value);
+                String urlString = SET_ENTRY_URL + "id=" + entry.getId() + ";title=" + value;
+                RequestBuilder builder = new RequestBuilder(RequestBuilder.PUT, URL.encode(urlString));
+
+                try {
+                    builder.sendRequest(null, new RequestCallback() {
+                        public void onError(Request request, Throwable e) {
+                            statusLabel.setText("Server Error: " + e.toString());
+                        }
+
+                        public void onResponseReceived(Request request, Response response) {
+                            if (response.getStatusCode() == 200) {
+                            } else {
+                                statusLabel.setText("Server Error: '"+response.getStatusCode() +"' "
+                                                    +response.getStatusText());
+                            }
+                        }
+                    });
+                } catch (RequestException e) {
+                    statusLabel.setText("Server Error: " + e.toString());
+                }
             }
         });
 
--- todolist_client/src/com/oci/example/todolist/client/TodoListEntry.java
+++ todolist_client/src/com/oci/example/todolist/client/TodoListEntry.java
@@ -1,36 +1,85 @@
 package com.oci.example.todolist.client;
 
-import com.google.gwt.core.client.JavaScriptObject;
 import java.util.Comparator;
 
 /**
  */
 
-class TodoListEntry extends JavaScriptObject {
+class TodoListEntry {
+    private final int id;
+    private String title;
+    private String notes = "";
+    private boolean complete = false;
 
-    protected TodoListEntry() {
+
+    TodoListEntry(int id, String title) {
+        this.id = id;
+        this.title = title;
     }
 
-    public final native int getId() /*-{
-        return this.id;
-    }-*/;
+    TodoListEntry(int id, String title, String notes) {
+        this.id = id;
+        this.title = title;
+        this.notes = notes;
+    }
 
-    public final native String getTitle() /*-{
-        return this.title;
-    }-*/;
+    TodoListEntry(int id, String title, String notes, boolean complete) {
+        this.id = id;
+        this.title = title;
+        this.notes = notes;
+        this.complete = complete;
+    }
 
-    public final native String getNotes() /*-{
-        return this.notes;
-    }-*/;
+    public int getId() {
+        return id;
+    }
 
-    public final native boolean isComplete() /*-{
-        return this.complete == 1;
-    }-*/;
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public boolean isComplete() {
+        return complete;
+    }
+
+    public void setComplete(boolean complete) {
+        this.complete = complete;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TodoListEntry that = (TodoListEntry) o;
+
+        return id == that.id;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+
+    static class CompareId implements Comparator<TodoListEntry> {
+
+        public int compare(TodoListEntry e1, TodoListEntry e2) {
+            return e1.getId() - e2.getId();
+        }
+    }
 }
 
-class TodoListEntryComparator implements Comparator<TodoListEntry> {
 
-     public int compare(TodoListEntry e1, TodoListEntry e2) {
-        return e1.getId() - e2.getId();
-     }
-}
