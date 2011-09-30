a6abca2603a9031338bf7ea935f0d3efa836b989
Jeff Clyne <jeffclyne@mindless.com>
2011-09-30 17:24:08 -0500

    gae implementation

--- todolist_server/todolist_server.py
+++ todolist_server/todolist_server.py
@@ -92,11 +92,13 @@
 """
 
 
-import web
-import json
-import time
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext import db
+from django.utils import simplejson as json
+from time import time
 from threading import Thread,Event
-from time import sleep
+
 
 __author__ = "Jeff Clyne"
 __copyright__ = "Copyright 2011, Object Computing Inc."
@@ -107,125 +109,36 @@ __maintainer__ = "Jeff Clyne"
 __email__ = "jeffclyne@mindless.com"
 __status__= "released"
 
-urls = (
-    '/todolist', 'Index',
-    '/todolist/entries', 'EntryListHandler',
-    '/todolist/entries/(\d+)', 'EntryHandler'
-)
 
-
-dbn= "sqlite"
-todolist_db= "todolist.db"
-entries_table="entries"
 
 archive_duration=86400.0 # 24 hours
 cleanup_interval=1800.0 # 30 minutes
 
 
-
-#db = web.database(dbn=dbn,user=user,passwd=passwd)
-#db.query("CREATE DATABASE IF NOT EXISTS "+todolist_db)
-db = web.database(dbn=dbn,db=todolist_db)
-
-# Execute the schema sql statements to setup the required tables
-#with open("schema.sql") as file:
-#    map(db.query,[stmt for stmt in file.read().translate(None,"\n\r").split(';') if stmt!="" ])
-
-
-class encode_response(object):
-    """ Method decorator that will encode returned responses, as well as
-    data in exceptions, into wire format
-    """
-
-    @staticmethod
-    def encode_json(object):
-        """ Encodes the specified object into JSON, using the
-         builtin JSON encoder. It also writes the correct content-type
-         into the response header
-        """
-        web.header('Content-Type', 'application/json')
-        if isinstance(object,web.utils.IterBetter):
-            object=tuple(object)
-
-        out=  json.dumps(object,indent=2)
-        return out
-
-    def __init__(self,encode_type):
-        self.encoder=getattr(self,"encode_"+encode_type)
-
-    def __call__(self,method):
-        def encode_wrapper(*args,**kwargs):
-            try:
-                return self.encoder(method(*args,**kwargs))
-            except web.HTTPError,e:
-                if hasattr(e,'data'):
-                    e.data=self.encoder(e.data)
-                raise e
-
-        return encode_wrapper
-
-class web_input(object):
-    """ Method decorator to parse the incoming web input, validate it,
-    and pass the values as keyword arguments to the wrapped method. The
-    valid params are passed in a list as well as a flag indicating whether
-    and empty parameter list is valid
-    """
-
-    @staticmethod
-    def decode_id(value):
-        return tuple([int(id) for id in value.split()])
-
-    @staticmethod
-    def decode_title(value):
-        return value
-
-    @staticmethod
-    def decode_notes(value):
-        return value
-
-    @staticmethod
-    def decode_modified(value):
-        return value
-
-    @staticmethod
-    def decode_complete(value):
-        flag = int(value)
-        if flag: return 1
-        return 0
-
-    def __init__(self,*valid_params):
-        self.valid_params=set(tuple(valid_params))
-
-    def __call__(self,method):
-        def validate_wrapper(*args,**kwargs):
-            input=dict(web.input())
-            if not (set(input.keys()) <= self.valid_params):
-                raise web.badrequest()
-
-            for key,value in input.items():
-                try:
-                    input[key]=getattr(self,"decode_"+key)(value)
-                except AttributeError:
-                    pass
-
-            kwargs.update(input)
-            return method(*args,**kwargs)
-
-        return validate_wrapper
-
-class Index(object):
+class Index(webapp.RequestHandler):
     """ Servlet to handle a redirect on the base module URL"""
     def GET(self):
-        raise web.seeother("static/todolist.html")
+        pass
 
 
-class EntryListHandler(object):
+def sqlors(key,values):
+    return " OR ".join([key+" = "+value for value in values])
+
+class TodolistEntry(db.Model):
+    title = db.StringProperty(required=True)
+    notes = db.StringProperty()
+    completed = db.BooleanProperty(required=True,default=False)
+    created = db.FloatProperty(required=True)
+    modified = db.FloatProperty(required=True)
+    deleted = db.BooleanProperty(required=True,default=False)
+
+    def to_dict(self):
+       return dict(self.__dict__)
+
+class EntryListHandler(webapp.RequestHandler):
     """ Servlet to handle the /todolist? URL"""
 
-
-    @encode_response('json')
-    @web_input('id','modified')
-    def GET(self,id=None,modified=None):
+    def get(self):
         """Retrieves the list of todolist entries,
 
         URI Params:
@@ -240,25 +153,37 @@ class EntryListHandler(object):
             400(bad request) - invalid query string  specified
         """
 
-        now = time.time()
+        now = time()
+        ids = self.request.get_all("id")
+        modified = self.request.get("modified",None)
         where=None
 
         if modified:
             if ( now - float(modified) ) > archive_duration:
-                raise web.badrequest
+                raise self.error(400)
 
             where="(modified > %s AND modified <= %f)"%(modified,now)
         else:
-            where="(deleted = 0)"
+            where="deleted = False"
 
-        if id:
-            where+=" AND ("+web.db.sqlors('id = ',id)+")"
+        if ids:
+            where+=" AND ("+sqlors('id',ids)+")"
 
-        return {"timestamp":now, "entries":tuple(db.select(entries_table,order='created', where=where))}
+        query = db.GqlQuery("SELECT * from TodolistEntry "+
+                            "WHERE "+where+ " "+
+                            "ORDER BY created ")
 
-    @encode_response('json')
-    @web_input('title', 'notes', 'complete')
-    def POST(self,**params):
+        res = tuple(query)
+        entries = tuple([r.to_dict() for r in res])
+
+        self.response.headers['Content-type'] = 'application/json'
+        self.response.out.write(
+                json.dumps({"timestamp":now, "entries":entries}
+                           ,indent=2) )
+
+        self.response.set_status(200)
+
+    def post(self):
         """Creates a new todolist entry
 
         URI Params:
@@ -274,16 +199,20 @@ class EntryListHandler(object):
             400(bad request) - invalid query string  specified
         """
 
-        now = time.time()
-        params.update( {"created":now, "modified":now} )
-        with db.transaction():
-            newid=db.insert( entries_table, **params )
-            new_entry = db.where( entries_table, id=newid, deleted=0 )[0]
+        now = time()
 
-        raise web.created(new_entry)
+        entry = TodolistEntry(title = self.request.get("title",None),
+                                notes = self.request.get("notes",None),
+                                completed = self.request.get("completed",False),
+                                created=now,modified=now)
 
-    @web_input('id')
-    def DELETE(self,id=None):
+        newid = entry.put()
+
+        self.response.headers['Content-type'] = 'application/json'
+        self.response.out.write({"id":newid.id()})
+        self.response.set_status(201)
+
+    def delete(self,id=None):
         """Deletes the specified list of entries
 
         URI Params:
@@ -297,19 +226,12 @@ class EntryListHandler(object):
         Status Codes:
             200(ok) - ok, body is empty
         """
-
-        now = time.time()
-        where="deleted = 0"
-        if id:
-            where+=" AND "+web.db.sqlors('id = ',id)
-
-        db.update(entries_table, where=where, deleted=1,modified=now)
+        pass
         
-class EntryHandler(object):
+class EntryHandler(webapp.RequestHandler):
     """ Servlet to handle the /todolist/entries/(\d+) URL"""
 
-    @encode_response('json')
-    def GET(self,id):
+    def get(self,id):
         """Returns a single todolist_entry, specified by id
 
         URI Params:
@@ -319,14 +241,10 @@ class EntryHandler(object):
             200(ok) - ok, body includes the todolist_entry
             410(gone) - entry with specified id does not exist
         """
-        try:
-            return db.where(entries_table,id=id,deleted=0)[0]
-        except IndexError:
-            raise web.gone()
+        pass
 
-    @encode_response('json')
-    @web_input('title', 'notes', 'complete')
-    def PUT(self,id,**params):
+
+    def put(self,id,**params):
         """Updates the specified entry, with the values specified
         in the query string, and returns the updated todolist_entry
 
@@ -344,21 +262,9 @@ class EntryHandler(object):
             410(gone) - entry with specified id does not exist
         """
 
-        now = time.time()
-        params.update( {"modified":now} )
-        data = web.data()
+        pass
 
-        with db.transaction():
-            db.update(entries_table,
-                      where='id=$id AND deleted=0',
-                      vars=locals(),
-                      **params)
-            try:
-                return db.where(entries_table,  id=id, deleted=0 )[0]
-            except IndexError:
-                raise web.gone()
-
-    def DELETE(self,id):
+    def delete(self,id):
         """Deletes the specified entry
 
         URI Params:
@@ -368,38 +274,13 @@ class EntryHandler(object):
             200(ok) - ok, body is empty
         """
 
-        now = time.time()
-        db.update(entries_table, where='id = $id AND deleted=0',vars=locals(),deleted=1,modified=now)
-
-class CleanDeletedEntriesTask (Thread):
-
-    def __init__(self,cutoff,interval):
-        Thread.__init__(self,name="CleanDeletedEntriesTask")
-        self.canceled=Event()
-        self.cutoff=cutoff
-        self.interval=interval
-
-    def performCleanup(self):
-        db.delete(entries_table,
-                  where="deleted = 1 AND modified < $cutoff",
-                  vars={"cutoff":time.time()-self.cutoff})
-
-    def run(self):
-        self.performCleanup()
-        while not self.canceled.wait(self.interval):
-            self.performCleanup()
+        pass
 
 
-    def cancel(self):
-        self.canceled.set()
-        self.join()
-
-cleanup_task = CleanDeletedEntriesTask(archive_duration,cleanup_interval)
-app=web.application(urls, globals())
+app = webapp.WSGIApplication([('/todolist', Index),
+                              ('/todolist/entries', EntryListHandler),
+                              ('/todolist/entries/(\d+)',EntryHandler)],
+                             debug=True)
 
 if __name__ == '__main__':
-    cleanup_task.start()
-    try:
-        app.run()
-    finally:
-        cleanup_task.cancel()
+    run_wsgi_app(app)
---
+++ todolist_server/todolist_server_orig.py
@@ -0,0 +1,403 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+""" Restful Web Protocol for Todo List
+
+    This implements a RESTful web protocol to manage a todo list. All the todo list
+    entries are persisted in a relational database store.  Resource responses are
+    encoded in JSON.
+
+    Protocol Definition:
+
+    Data Format:
+        todolist_entry object
+            {
+              "id": entry ID,
+              "title": title of the Entry,
+              "notes": notes associated with the entry,
+              "complete": flag indicating whether the entry is complete
+            }
+        todolist_entry array
+            [
+              {
+                "id": entry ID,
+                "title": title of the Entry,
+                "notes": notes associated with the entry,
+                "complete": flag indicating whether the entry is complete
+              },
+              {
+                "id": entry ID,
+                "title": title of the Entry,
+                "notes": notes associated with the entry,
+                "complete": flag indicating whether the entry is complete
+              },
+              ...
+            ]
+
+    Status Codes:
+        200(ok) - request was successful
+        201(created) - new entry as been created
+        400(bad request) - invalid query string
+        410(gone) - entry does not exist
+
+
+    URIs:
+        todolist/entries - list of todolist entries
+            GET
+                Format - todolist_entry array
+                Query Parameters = id (e.g. '?id=1' or '?id=1+2+5')
+                    NOTE: Omitting the id parameter will retrieve all entries
+                Status Codes - 200,400
+
+            POST
+                Format - todolist_entry
+                Query Parameters = title,notes,completed (e.g. '?title=ENTRY' or '?title=ENTRY;notes=NOTES')
+                Status Codes - 201,400
+
+            DELETE
+                Format - empty
+                Query Parameters = id (e.g. '?id=1' or '?id=1+2+5')
+                Status Codes - 200
+
+        todolist/entries/<id> - single todolist entry, referenced by entry id
+            GET
+                Format - todolist_entry
+                Status Codes - 200,410
+
+            PUT
+                Format - todolist_entry
+                Query Parameters = title,notes,completed (e.g. '?title=ENTRY' or '?title=ENTRY;notes=NOTES')
+                Status Codes - 200,400,410
+
+            DELETE
+                Format - empty
+                Status Codes - 200
+
+"""
+
+
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import util
+from django.utils import simplejson as json
+from threading import Thread,Event
+
+
+__author__ = "Jeff Clyne"
+__copyright__ = "Copyright 2011, Object Computing Inc."
+__credits__ = ["Jeff Clyne"]
+__license__ = "GPL"
+__version__ = "1.0"
+__maintainer__ = "Jeff Clyne"
+__email__ = "jeffclyne@mindless.com"
+__status__= "released"
+
+
+archive_duration=86400.0 # 24 hours
+cleanup_interval=1800.0 # 30 minutes
+
+
+# Execute the schema sql statements to setup the required tables
+#with open("schema.sql") as file:
+#    map(db.query,[stmt for stmt in file.read().translate(None,"\n\r").split(';') if stmt!="" ])
+
+
+class encode_response(object):
+    """ Method decorator that will encode returned responses, as well as
+    data in exceptions, into wire format
+    """
+
+    @staticmethod
+    def encode_json(object):
+        """ Encodes the specified object into JSON, using the
+         builtin JSON encoder. It also writes the correct content-type
+         into the response header
+        """
+        web.header('Content-Type', 'application/json')
+        if isinstance(object,web.utils.IterBetter):
+            object=tuple(object)
+
+        out=  json.dumps(object,indent=2)
+        return out
+
+    def __init__(self,encode_type):
+        self.encoder=getattr(self,"encode_"+encode_type)
+
+    def __call__(self,method):
+        def encode_wrapper(*args,**kwargs):
+            try:
+                return self.encoder(method(*args,**kwargs))
+            except web.HTTPError,e:
+                if hasattr(e,'data'):
+                    e.data=self.encoder(e.data)
+                raise e
+
+        return encode_wrapper
+
+class web_input(object):
+    """ Method decorator to parse the incoming web input, validate it,
+    and pass the values as keyword arguments to the wrapped method. The
+    valid params are passed in a list as well as a flag indicating whether
+    and empty parameter list is valid
+    """
+
+    @staticmethod
+    def decode_id(value):
+        return tuple([int(id) for id in value.split()])
+
+    @staticmethod
+    def decode_title(value):
+        return value
+
+    @staticmethod
+    def decode_notes(value):
+        return value
+
+    @staticmethod
+    def decode_modified(value):
+        return value
+
+    @staticmethod
+    def decode_complete(value):
+        flag = int(value)
+        if flag: return 1
+        return 0
+
+    def __init__(self,*valid_params):
+        self.valid_params=set(tuple(valid_params))
+
+    def __call__(self,method):
+        def validate_wrapper(*args,**kwargs):
+            input=dict(web.input())
+            if not (set(input.keys()) <= self.valid_params):
+                raise web.badrequest()
+
+            for key,value in input.items():
+                try:
+                    input[key]=getattr(self,"decode_"+key)(value)
+                except AttributeError:
+                    pass
+
+            kwargs.update(input)
+            return method(*args,**kwargs)
+
+        return validate_wrapper
+
+class Index(object):
+    """ Servlet to handle a redirect on the base module URL"""
+    def GET(self):
+        raise web.seeother("static/todolist.html")
+
+
+class EntryListHandler(object):
+    """ Servlet to handle the /todolist? URL"""
+
+
+    @encode_response('json')
+    @web_input('id','modified')
+    def GET(self,id=None,modified=None):
+        """Retrieves the list of todolist entries,
+
+        URI Params:
+            None
+
+        Query String Params:
+            id - entry id to include in result
+                NOTE: this parameter can have multiple values
+
+        Status Codes:
+            200(ok) - ok, body includes the todolist_entry list
+            400(bad request) - invalid query string  specified
+        """
+
+        now = time.time()
+        where=None
+
+        if modified:
+            if ( now - float(modified) ) > archive_duration:
+                raise web.badrequest
+
+            where="(modified > %s AND modified <= %f)"%(modified,now)
+        else:
+            where="(deleted = 0)"
+
+        if id:
+            where+=" AND ("+web.db.sqlors('id = ',id)+")"
+
+        return {"timestamp":now, "entries":tuple(db.select(entries_table,order='created', where=where))}
+
+    @encode_response('json')
+    @web_input('title', 'notes', 'complete')
+    def POST(self,**params):
+        """Creates a new todolist entry
+
+        URI Params:
+            None
+
+        Query String Params:
+            title - string contaning entry title
+            notes - string contaning entry notes
+            complete - boolean flag indicating the entry is completed
+
+        Status Codes:
+            201(created) -  body includes the new todolist_entry
+            400(bad request) - invalid query string  specified
+        """
+
+        now = time.time()
+        params.update( {"created":now, "modified":now} )
+        with db.transaction():
+            newid=db.insert( entries_table, **params )
+            new_entry = db.where( entries_table, id=newid, deleted=0 )[0]
+
+        raise web.created(new_entry)
+
+    @web_input('id')
+    def DELETE(self,id=None):
+        """Deletes the specified list of entries
+
+        URI Params:
+            none
+
+        Query String Params:
+            id - entry id to delete
+                NOTE: this parameter can have multiple values
+                NOTE: If omitted, all todolist entries will be deleted
+
+        Status Codes:
+            200(ok) - ok, body is empty
+        """
+
+        now = time.time()
+        where="deleted = 0"
+        if id:
+            where+=" AND "+web.db.sqlors('id = ',id)
+
+        db.update(entries_table, where=where, deleted=1,modified=now)
+
+class EntryHandler(object):
+    """ Servlet to handle the /todolist/entries/(\d+) URL"""
+
+    @encode_response('json')
+    def GET(self,id):
+        """Returns a single todolist_entry, specified by id
+
+        URI Params:
+            id - entry id of the entry to return
+
+        Status Codes:
+            200(ok) - ok, body includes the todolist_entry
+            410(gone) - entry with specified id does not exist
+        """
+        try:
+            return db.where(entries_table,id=id,deleted=0)[0]
+        except IndexError:
+            raise web.gone()
+
+    @encode_response('json')
+    @web_input('title', 'notes', 'complete')
+    def PUT(self,id,**params):
+        """Updates the specified entry, with the values specified
+        in the query string, and returns the updated todolist_entry
+
+        URI Params:
+            id - entry id of the entry to update
+
+        Query String Params:
+            title - string contaning entry title
+            notes - string contaning entry notes
+            complete - boolean flag indicating the entry is completed
+
+        Status Codes:
+            200(ok) - ok, body includes the updated todolist_entry
+            400(bad request) - invalid query string  specified
+            410(gone) - entry with specified id does not exist
+        """
+
+        now = time.time()
+        params.update( {"modified":now} )
+        data = web.data()
+
+        with db.transaction():
+            db.update(entries_table,
+                      where='id=$id AND deleted=0',
+                      vars=locals(),
+                      **params)
+            try:
+                return db.where(entries_table,  id=id, deleted=0 )[0]
+            except IndexError:
+                raise web.gone()
+
+    def DELETE(self,id):
+        """Deletes the specified entry
+
+        URI Params:
+            id - entry id of the entry to delete
+
+        Status Codes:
+            200(ok) - ok, body is empty
+        """
+
+        now = time.time()
+        db.update(entries_table, where='id = $id AND deleted=0',vars=locals(),deleted=1,modified=now)
+
+class CleanDeletedEntriesTask (Thread):
+
+    def __init__(self,cutoff,interval):
+        Thread.__init__(self,name="CleanDeletedEntriesTask")
+        self.canceled=Event()
+        self.cutoff=cutoff
+        self.interval=interval
+
+    def performCleanup(self):
+        db.delete(entries_table,
+                  where="deleted = 1 AND modified < $cutoff",
+                  vars={"cutoff":time.time()-self.cutoff})
+
+    def run(self):
+        self.performCleanup()
+        while not self.canceled.wait(self.interval):
+            self.performCleanup()
+
+
+    def cancel(self):
+        self.canceled.set()
+        self.join()
+
+cleanup_task = CleanDeletedEntriesTask(archive_duration,cleanup_interval)
+
+urls = (
+    '/todolist', 'Index',
+    '/todolist/entries', 'EntryListHandler',
+    '/todolist/entries/(\d+)', 'EntryHandler'
+)
+
+def main():
+    application = webapp.WSGIApplication(
+                        [   ('/todolist', Index),
+                            ('/todolist/entries',EntryListHandler),
+                            ('/todolist/entries/(\d+)',EntryHandler)  ]
+                        ,debug=True)
+
+    util.run_wsgi_app(application)
+
+
+if __name__ == '__main__':
+    #cleanup_task.start()
+    #try:
+        main()
+    #finally:
+    #    cleanup_task.cancel()
